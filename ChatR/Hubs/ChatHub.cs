a48289ababdfd127f07951d11017614c9fca643b
Dragan89 <Archaic.dragan@gmail.com>
2014-08-04 17:38:23 +0200

    Extracted message in separate object

--- ChatR/Hubs/ChatHub.cs
+++ ChatR/Hubs/ChatHub.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+using ChatR.Models;
 
 namespace ChatR.Hubs
 {
     public class ChatHub : Hub
     {
-        public void Send(string name, string message)
+        public void Send(Message message)
         {
             // Call the addNewMessageToPage method to update clients.
-            Clients.All.addNewMessageToPage(name, message);
+            Clients.All.addNewMessageToPage(message);
         }
+
+        public override Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected()
+        {
+            return base.OnDisconnected();
+        }
+
+        public override Task OnReconnected()
+        {
+            return base.OnReconnected();
+        }
+
     }
 }
---
+++ ChatR/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatR.Models
+{
+    public class Message
+    {
+        public string Name { get; set; }
+        public string MessageText { get; set; }
+    }
+}
